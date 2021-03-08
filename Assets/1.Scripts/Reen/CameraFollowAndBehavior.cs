ccf0cdc8e6840df58e1028f1a51ccbc88f404efc
Reenly <69853981+Reenly@users.noreply.github.com>
2021-03-08 15:25:08 +0200

    BUGFIX Camera not properly following player, player-movement Animation in-progress

---
+++ Assets/1.Scripts/Reen/CameraFollowAndBehavior.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowAndBehavior : MonoBehaviour
+{
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(GameObject.Find("PlayerAvatar").GetComponent<Transform>().position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+    }
+}
