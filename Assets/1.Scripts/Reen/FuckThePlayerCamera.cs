05e7ce22512def7a3dab9f3af33d4c6a209c75be
Reenly <69853981+Reenly@users.noreply.github.com>
2021-03-08 20:04:52 +0200

    Added CameraAids, fuck the players
    Going right - flips your camera around
    Going left - restores default
    Jumping - flips your camera upside down

---
+++ Assets/1.Scripts/Reen/FuckThePlayerCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuckThePlayerCamera : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            gameObject.transform.rotation = new Quaternion(0f,180f, gameObject.transform.rotation.z, 0f);
+            gameObject.transform.position = new Vector3(0f, 0f, 1f);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            gameObject.transform.rotation = new Quaternion(0f, 0f, gameObject.transform.rotation.z, 0f);
+            gameObject.transform.position = new Vector3(0f, 0f, -1f);
+        }
+
+        if (Input.GetAxis("Vertical") > 0 || Input.GetKey(KeyCode.Space))
+        {
+            gameObject.transform.rotation = new Quaternion(0f, gameObject.transform.rotation.y, 180f, 0f);
+            gameObject.transform.position = new Vector3(0f, 0f, -1f);
+        }
+        else if (Input.GetAxis("Vertical") > 0 && Input.GetAxis("Vertical") < 0.21)
+        {
+            gameObject.transform.rotation = new Quaternion(0f, gameObject.transform.rotation.y, 0f, 0f);
+            gameObject.transform.position = new Vector3(0f, 0f, -1f);
+        }
+    }
+}
