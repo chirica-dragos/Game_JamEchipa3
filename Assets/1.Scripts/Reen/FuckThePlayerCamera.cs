3f9f43d12aff2c6e355b2da5ed1c29268237f3e6
Reenly <69853981+Reenly@users.noreply.github.com>
2021-03-09 14:33:15 +0200

    GameComplete

--- Assets/1.Scripts/Reen/FuckThePlayerCamera.cs
+++ Assets/1.Scripts/Reen/FuckThePlayerCamera.cs
@@ -6,26 +6,37 @@ public class FuckThePlayerCamera : MonoBehaviour
 {
     void Update()
     {
-        if(Input.GetAxis("Horizontal") > 0)
+        if (GetComponent<CameraFollowAndBehavior>().follow == true)
         {
-            gameObject.transform.rotation = new Quaternion(0f,180f, gameObject.transform.rotation.z, 0f);
-            gameObject.transform.position = new Vector3(0f, 0f, 1f);
-        }
-        else if (Input.GetAxis("Horizontal") < 0)
-        {
-            gameObject.transform.rotation = new Quaternion(0f, 0f, gameObject.transform.rotation.z, 0f);
-            gameObject.transform.position = new Vector3(0f, 0f, -1f);
-        }
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                gameObject.transform.rotation = new Quaternion(0f, 180f, gameObject.transform.rotation.z, 0f);
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x, 0f, 1f);
+                gameObject.GetComponentInChildren<SpriteRenderer>().flipX = true;
+                gameObject.GetComponentInChildren<SpriteRenderer>().flipY = false;
+            }
+            else if (Input.GetAxis("Horizontal") < 0)
+            {
+                gameObject.transform.rotation = new Quaternion(0f, 0f, gameObject.transform.rotation.z, 0f);
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x, 0f, -1f);
+                gameObject.GetComponentInChildren<SpriteRenderer>().flipX = false;
+                gameObject.GetComponentInChildren<SpriteRenderer>().flipY = false;
+            }
 
-        if (Input.GetAxis("Vertical") > 0 || Input.GetKey(KeyCode.Space))
-        {
-            gameObject.transform.rotation = new Quaternion(0f, gameObject.transform.rotation.y, 180f, 0f);
-            gameObject.transform.position = new Vector3(0f, 0f, -1f);
-        }
-        else if (Input.GetAxis("Vertical") > 0 && Input.GetAxis("Vertical") < 0.21)
-        {
-            gameObject.transform.rotation = new Quaternion(0f, gameObject.transform.rotation.y, 0f, 0f);
-            gameObject.transform.position = new Vector3(0f, 0f, -1f);
+            if (Input.GetAxis("Vertical") > 0 || Input.GetKey(KeyCode.Space))
+            {
+                gameObject.transform.rotation = new Quaternion(0f, gameObject.transform.rotation.y, 180f, 0f);
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x, 0f, -1f);
+                gameObject.GetComponentInChildren<SpriteRenderer>().flipX = false;
+                gameObject.GetComponentInChildren<SpriteRenderer>().flipY = true;
+            }
+            else if (Input.GetAxis("Vertical") > 0 && Input.GetAxis("Vertical") < 0.21)
+            {
+                gameObject.transform.rotation = new Quaternion(0f, gameObject.transform.rotation.y, 0f, 0f);
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x, 0f, -1f);
+                gameObject.GetComponentInChildren<SpriteRenderer>().flipX = false;
+                gameObject.GetComponentInChildren<SpriteRenderer>().flipY = false;
+            }
         }
     }
 }
