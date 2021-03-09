9f20836449b43b945622ea3857258c9c9ddae434
chirica-dragos <67731768+chirica-dragos@users.noreply.github.com>
2021-03-09 15:33:11 +0200

    Revert "Revert "Merge branch 'Reen' into main""
    This reverts commit 092611c4c24a23c65463937ee006dfab1ecddd36.

--- Assets/1.Scripts/Reen/CameraFollowAndBehavior.cs
+++ Assets/1.Scripts/Reen/CameraFollowAndBehavior.cs
@@ -11,8 +11,16 @@ using UnityEngine;
 
 public class CameraFollowAndBehavior : MonoBehaviour
 {
+
+    public bool follow = true;
     void Update()
     {
-        gameObject.transform.position = new Vector3(GameObject.Find("PlayerAvatar").GetComponent<Transform>().position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        if (follow == true)
+        {
+            gameObject.transform.position = new Vector3(GameObject.Find("PlayerAvatar").GetComponent<Transform>().position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+            GetComponent<AudioSource>().pitch = 1f;
+        }
+        else
+            GetComponent<AudioSource>().pitch = 0.72f;
     }
 }
