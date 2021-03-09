ae58c56061855c375d980244078243064eea5504
silviuhogas <silviu.hogas@yahoo.com>
2021-03-09 18:11:05 +0200

    sunet deblocat usa cand poti da click

--- Assets/1.ScriptsSilviu/MouseOverCheck.cs
+++ Assets/1.ScriptsSilviu/MouseOverCheck.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class MouseOverCheck : MonoBehaviour
 {
     public  int canClickButtons = 0;
+    public GameObject unlockeddoorsound;
     public void OnMouseEnter()
     {
         Debug.Log("aintrat");
         canClickButtons = 1;
+        unlockeddoorsound.SetActive(true);
         
     }
 
